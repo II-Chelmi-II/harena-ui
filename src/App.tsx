cf57ab49ba17839f310128a5d5acd440162e87f7
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-07-28 18:05:21 +0300

    chore: log dataProvider for debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { Admin, Datagrid, DataProvider, List, Resource, TextField } from 'react-admin';
-import patrimoineProvider from './providers/patrimoineProvider';
+import { Admin, DataProvider, Resource } from "react-admin";
+import dataProvider from "./providers/dataProvider";
+import PossessionList from "./components/PossessionList";
+import PatrimoineList from "./components/PatrimoineList";
 
-const PatrimoineList = () => (
-  <List>
-    <Datagrid>
-      <TextField source="nom" label="Nom"/>
-      <TextField source="possesseur.nom" label="Possesseur"/>
-      <TextField source="t" label="Date"/>
-      <TextField source="valeur_comptable" label="Valeur comptable"/>
-    </Datagrid>
-  </List>
-);
+// Logging
+dataProvider
+  .getList("patrimoines", { pagination: { page: 1, perPage: 10 } })
+  .then((data: any) => console.log(data))
+  .catch((error: any) => console.error(error));
+dataProvider
+  .getList("possessions", { pagination: { page: 1, perPage: 10 } })
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
 
 function App() {
   return (
-    <Admin dataProvider={patrimoineProvider as unknown as DataProvider}>
+    <Admin dataProvider={dataProvider as DataProvider}>
       <Resource name="patrimoines" list={PatrimoineList} />
+      <Resource name="possessions" list={PossessionList} />
     </Admin>
-  )
+  );
 }
 
-export default App
+export default App;
