969ed8d04eb0c140eb015d8efb3f1f224401fdc0
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-07-29 13:36:10 +0300

    build: Update cypress config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
+import webpackConfig from "./webpack.config"; 
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/index.ts',
+    baseUrl: "http://localhost:5173",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/index.ts",
   },
+
   retries: {
     runMode: 2,
     openMode: 1,
   },
+
   screenshotOnRunFailure: true,
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+      webpackConfig: webpackConfig,
+    },
+  },
 });
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+import path from 'path';
+
+export default {
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
